f6a72f631378161f7686e31da53deb1c8a308409
Mr_Spaar <julienpistre.cagnes@gmail.com>
2020-08-28 22:30:32 +0200

    Unload command

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from os import environ
 
 bot = commands.Bot(command_prefix=['!', '$'])
 bot.remove_command('help')
-initial_extensions = ['cogs.admin', 'cogs.music', 'cogs.chat', 'cogs.random', 'cogs.weather', 'cogs.logs']
+initial_extensions = ['cogs.admin', 'cogs.music', 'cogs.chat', 'cogs.random', 'cogs.weather']
 
 if __name__ == '__main__':
     for extension in initial_extensions:
@@ -15,5 +15,9 @@ async def on_ready():
     print(f'\nLogged as: {bot.user.name} - {bot.user.id}')
     await bot.change_presence(activity=Game(name='!help'))
     print(f'Bot is ready to go!')
+
+@bot.command(hidden=True)
+async def reload(ctx, extension):
+    bot.reload_extension(extension)
 
 bot.run(environ['BOT_TOKEN'], bot=True, reconnect = True)
